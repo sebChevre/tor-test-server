cc457105a504cff9d461da72fdea990ccc2b7703
Sébastien Chèvre <seb.chevre@gmail.com>
2019-12-11 13:03:53 +0100

    ok

---
+++ src/main/java/ch/sebooom/tor/testserver/TorTestApplication.java
@@ -0,0 +1,13 @@
+package ch.sebooom.tor.testserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorTestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TorTestApplication.class, args);
+    }
+}
